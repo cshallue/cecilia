96d1499ac358c70d1b1c331923ccc09a9e166fad
Chris Shallue <chris.shallue@gmail.com>
2025-03-03 16:59:33 -0500

    Add configuration.py

---
+++ cecilia/configuration.py
@@ -0,0 +1,20 @@
+import ml_collections
+
+
+def default():
+  return ml_collections.ConfigDict({
+      # Data.
+      "log_transform_y": False,
+      "normalize_y": True,
+      # Model.
+      "dim_input": 17,
+      "n_hidden": 2,
+      "dim_hidden": 1024,
+      "hidden_activation": "relu",
+      "dim_output": 16,
+      # Training.
+      "learning_rate": 1e-3,
+      "momentum": 0.9,
+      "num_epochs": 5,
+      "batch_size": 512,
+  })
