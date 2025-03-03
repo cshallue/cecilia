4df94f48f9eb317320295d1d0c289484dcd95bf1
Chris Shallue <chris.shallue@gmail.com>
2025-03-03 17:37:38 -0500

    Add tuning code

---
+++ cecilia/tuning/search_space.py
@@ -0,0 +1,131 @@
+import itertools
+
+import numpy as np
+from tensorboard.plugins.hparams import api as hp
+
+
+class SearchSpaceAxis:
+
+  def tensorboard_spec(self):
+    raise NotImplementedError
+
+
+class SearchSpace:
+
+  def __init__(self):
+    self.axes = {}
+
+  def get_tensorboard_specs(self):
+    return [
+        hp.HParam(name, ax.tensorboard_spec())
+        for name, ax in self.axes.items()
+    ]
+
+  def search(self):
+    return NotImplementedError
+
+
+# Random search
+
+
+class RandomParam(SearchSpaceAxis):
+
+  def __init__(self, rng):
+    self.rng = rng
+
+  def sample(self):
+    raise NotImplementedError
+
+
+class DiscreteParam(RandomParam):
+
+  def __init__(self, rng, values):
+    super().__init__(rng)
+    self.values = values
+
+  def sample(self):
+    return self.rng.choice(self.values)
+
+  def tensorboard_spec(self):
+    return hp.Discrete(self.values)
+
+
+class RealParam(RandomParam):
+
+  def __init__(self, rng, low, high):
+    super().__init__(rng)
+    self.low = low
+    self.high = high
+
+  def tensorboard_spec(self):
+    return hp.RealInterval(self.low, self.high)
+
+
+class UniformParam(RealParam):
+
+  def sample(self):
+    return self.rng.uniform(self.low, self.high)
+
+
+class LogUniformParam(RealParam):
+
+  def sample(self):
+    log_x = self.rng.uniform(np.log(self.low), np.log(self.high))
+    return np.exp(log_x)
+
+
+class RandomSearchSpace(SearchSpace):
+
+  def __init__(self, rng=None):
+    super().__init__()
+
+    if rng is None:
+      rng = np.random.default_rng()
+
+    self.rng = rng
+
+  def add_axis(self, name, param):
+    self.axes[name] = param
+
+  def add_discrete_param(self, name, values):
+    self.add_axis(name, DiscreteParam(self.rng, values))
+
+  def add_uniform_param(self, name, low, high):
+    self.add_axis(name, UniformParam(self.rng, low, high))
+
+  def add_log_uniform_param(self, name, low, high):
+    self.add_axis(name, LogUniformParam(self.rng, low, high))
+
+  def search(self, n=None):
+    while True:
+      yield {name: ax.sample() for name, ax in self.axes.items()}
+
+
+# Grid search
+
+
+class GridSearchAxis(SearchSpaceAxis):
+
+  def __init__(self, values, tb_type):
+    self.values = values
+    self.tb_type = tb_type
+
+  def tensorboard_spec(self):
+    if self.tb_type == "discrete":
+      return hp.Discrete(self.values)
+
+    if self.tb_type == "continuous":
+      return hp.RealInterval(np.min(self.values), np.max(self.values))
+
+    raise ValueError(self.tb_type)
+
+
+class GridSearchSpace(SearchSpace):
+
+  def add_axis(self, name, values, tb_type="discrete"):
+    self.axes[name] = GridSearchAxis(values, tb_type)
+
+  def search(self, n=None):
+    for gridpoint in itertools.product(*(ax.values
+                                         for ax in self.axes.values())):
+      yield {name: value for name, value in zip(self.axes.keys(), gridpoint)}
---
+++ cecilia/tuning/study.py
@@ -0,0 +1,95 @@
+import copy
+import os
+import shutil
+
+import numpy as np
+import tensorflow as tf
+from tensorboard.plugins.hparams import api as hp
+
+from . import training
+
+TB_METRICS = {
+    'loss': 'train_loss',
+    'val_loss': 'test_loss',
+    'train_mean_squared_error': 'train_MSE',
+    'test_mean_squared_error': 'test_MSE',
+    'train_mean_abs_error': 'train_MAE',
+    'test_mean_abs_error': 'test_MAE',
+    'train_mean_rel_error': 'train_MRE',
+    'test_mean_rel_error': 'test_MRE'
+}
+
+
+def _log_metrics_df(metrics_df):
+  for epoch, row in metrics_df.iterrows():
+    for name in metrics_df.columns:
+      tf.summary.scalar(name, row[name], step=epoch)
+
+
+def run_tuning_study(study_dir,
+                     base_config,
+                     search_space,
+                     train_df,
+                     test_df,
+                     X_cols,
+                     Y_cols,
+                     n_trials=None,
+                     overwrite=False):
+  if os.path.exists(study_dir):
+    if overwrite:
+      print(f"Removing existing output directory: {study_dir}")
+      shutil.rmtree(study_dir)
+    else:
+      raise ValueError(f"Output directory exists: {study_dir}")
+
+  # Log information for TensorBoard in the top level directory.
+  with tf.summary.create_file_writer(study_dir).as_default():
+    hp.hparams_config(hparams=search_space.get_tensorboard_specs(),
+                      metrics=[
+                          hp.Metric(name, display_name=dname)
+                          for name, dname in TB_METRICS.items()
+                      ])
+
+  # Run trials.
+  for n, trial_params in enumerate(search_space.search()):
+    trial_id = str(n)
+    trial_dir = os.path.join(study_dir, trial_id)
+    if os.path.exists(trial_dir):
+      continue  # Already done.
+
+    print("Trial", n)
+
+    # Sample new point in the search space.
+    config = copy.deepcopy(base_config)
+    config_updates = {}
+    for name, value in trial_params.items():
+      if name.startswith("one_minus_"):
+        name = name[len("one_minus_"):]
+        value = 1.0 - value
+      config_updates[name] = value
+    config.update(config_updates)
+    print("Params:", config_updates)
+
+    # Run the trial.
+    history, eval_metrics = training.train_model(config,
+                                                 train_df,
+                                                 test_df,
+                                                 X_cols,
+                                                 Y_cols,
+                                                 trial_dir,
+                                                 save_model=False)
+
+    # Log to Tensorboard.
+    with tf.summary.create_file_writer(trial_dir).as_default():
+      hp.hparams(trial_params,
+                 trial_id=trial_id)  # record the values used in this trial
+      _log_metrics_df(history)
+      for dataset, metrics_df in eval_metrics.items():
+        for metric in metrics_df.columns:
+          epoch = config.num_epochs - 1
+          avg_value = np.mean(metrics_df[metric])
+          tf.summary.scalar(f"{dataset}_{metric}", avg_value, step=epoch)
+
+    print()
+    if n_trials and n >= n_trials - 1:
+      break
