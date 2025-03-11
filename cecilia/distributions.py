60358a6c8291a9d229c04f7418610726eaadba9d
Chris Shallue <chris.shallue@gmail.com>
2025-03-11 12:23:25 -0400

    Identify dummy distributions

--- cecilia/distributions.py
+++ cecilia/distributions.py
@@ -1,30 +1,40 @@
 """Utilities for dealing with probability distributions."""
 
 import tensorflow as tf
+from tensorflow import keras
 
 # Enums that identify different distributions.
 NORMAL = tf.constant(0, dtype=tf.int32)
 LOG_NORMAL = tf.constant(1, dtype=tf.int32)
 
 
-def get_name(dist_key):
-  if dist_key == NORMAL:
-    return "Normal"
-  if dist_key == LOG_NORMAL:
-    return "LogNormal"
-
-  raise ValueError(f"Unrecognized distribution key: {dist_key}")
-
-
 def is_distribution(dist):
   return isinstance(dist, dict) and "distribution" in dist
 
 
-def validate(dist, copy=False):
+# Identify dummy distributions used when building model layers.
+def is_dummy(dist):
+  return isinstance(dist["distribution"], keras.KerasTensor)
+
+
+def get_name(dist):
+  if is_dummy(dist):
+    return "Dummy"
+
+  key = dist["distribution"]
+
+  if key == NORMAL:
+    return "Normal"
+
+  if key == LOG_NORMAL:
+    return "LogNormal"
+
+  raise ValueError(f"Unrecognized distribution key: {key}")
+
+
+def validate(dist):
   if set(dist.keys()) != {"distribution", "loc", "scale"}:
-    raise ValueError("Keys should be 'distribution', 'loc', 'scale'")
+    raise ValueError(
+        "Distribution keys should be {'distribution', 'loc', 'scale'}")
 
-  if copy:
-    dist = dist.copy()  # Shallow copy
-
-  return dist
+  _ = get_name(dist)  # Validate key.
